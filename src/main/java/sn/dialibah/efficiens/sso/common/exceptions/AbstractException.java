7f76d8156ce87d433f695b066a2a999a536cd3aa
osow <ousmane.sow@neo9.fr>
2017-02-17 15:27:36 +0100

    config(exceptions) : an abstractException for ResponseEntities

---
+++ src/main/java/sn/dialibah/efficiens/sso/common/exceptions/AbstractException.java
@@ -0,0 +1,57 @@
+package sn.dialibah.efficiens.sso.common.exceptions;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * by osow on 17/02/17.
+ * for neo9
+ */
+public abstract class AbstractException extends RuntimeException {
+	protected ResponseEntity forwardedResponse;
+
+	public AbstractException(String message) {
+		super(message);
+	}
+
+	public AbstractException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * Constructor to build {@link AbstractException} with a message and existing {@link ResponseEntity}
+	 *
+	 * @param message        indication {@link String} to be displayed to end user
+	 * @param responseEntity {@link ResponseEntity} to be returned by api
+	 */
+	public AbstractException(String message, ResponseEntity responseEntity) {
+		super(message);
+		this.forwardedResponse = responseEntity;
+	}
+
+	public abstract HttpStatus getStatus();
+
+	public abstract String getErrorCode();
+
+	public ResponseEntity getForwardedResponse() {
+		return this.forwardedResponse;
+	}
+
+	/**
+	 * Check if exception contains inner forwarded response to process
+	 *
+	 * @return true if {@code forwardedResponse} is not empty, false else
+	 */
+	public boolean hasForwardedResponse() {
+		return this.forwardedResponse != null;
+	}
+
+	public String getDeveloperMessage() {
+		StringBuilder sb = new StringBuilder(getMessage());
+		if (getCause() != null &&
+						StringUtils.isNotBlank(getCause().getMessage()))
+			sb.append(':').append(getCause().getMessage());
+		return sb.toString();
+	}
+}
